a89562fcc3fa545b7504c0d502ed4a4592c0b6a6
ilhamsyah <ilhamsyah809@gmail.com>
2016-04-09 04:10:37 +0700

    term dan perizinan

---
+++ protected/views/tahunAjaran/update.php
@@ -0,0 +1,21 @@
+<?php
+/* @var $this TahunAjaranController */
+/* @var $model TahunAjaran */
+
+$this->breadcrumbs=array(
+	'Tahun Ajarans'=>array('index'),
+	$model->id=>array('view','id'=>$model->id),
+	'Update',
+);
+
+$this->menu=array(
+	array('label'=>'List TahunAjaran', 'url'=>array('index')),
+	array('label'=>'Create TahunAjaran', 'url'=>array('create')),
+	array('label'=>'View TahunAjaran', 'url'=>array('view', 'id'=>$model->id)),
+	array('label'=>'Manage TahunAjaran', 'url'=>array('admin')),
+);
+?>
+
+<h1>Update TahunAjaran <?php echo $model->id; ?></h1>
+
+<?php $this->renderPartial('_form', array('model'=>$model)); ?>
