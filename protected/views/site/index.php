0548cd6b94ab08e2110e8849008df69e6a06bc38
parput <paramitha.putri@live.com>
2016-04-11 07:44:26 +0700

    login update

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -4,7 +4,7 @@
 $this->pageTitle=Yii::app()->name;
 ?>
 
-<<<<<<< HEAD
+
 <h1>Welcome to  <i><?php echo CHtml::encode(Yii::app()->name); ?>, <?php echo CHtml::encode(Yii::app()->user->name) ?></i></h1>
 
 
@@ -23,4 +23,4 @@ $this->pageTitle=Yii::app()->name;
 the <a href="http://www.yiiframework.com/doc/">documentation</a>.
 Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
 should you have any questions.</p> -->
->>>>>>> parput
+
