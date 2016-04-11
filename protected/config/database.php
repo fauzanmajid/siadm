59effe3cb966ca0739d2a763dd3721e275a7951f
Azza Firdaus <Azza Firdaus>
2016-04-11 22:25:10 +0700

    coba

--- protected/config/database.php
+++ protected/config/database.php
@@ -2,10 +2,9 @@
 
 // This is the database connection configuration.
 return array(
-	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	//'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 	// uncomment the following lines to use a MySQL database
-	
-	'connectionString' => 'mysql:host=localhost;dbname=siadmdb',
+	'connectionString' => 'mysql:host=localhost:3306;dbname=siadmdb',
 	'emulatePrepare' => true,
 	'username' => 'root',
 	'password' => '',
