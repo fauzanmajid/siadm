b2656f91840c4711e3c22881326a84aa3b6d9436
parput <paramitha.putri@live.com>
2016-04-12 22:05:17 +0700

    hiks

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -57,18 +57,15 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				//array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
-				//array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Halaman Muka', 'url'=>array('/site/index')),
+				array('label'=>'Pengguna', 'url'=>array('/User')),
+				array('label'=>'Data Santri', 'url'=>array('/Santri')),
 				array('label'=>'Prestasi', 'url'=>array('/prestasi')),
-				array('label'=>'Riwayat Penyakit', 'url'=>array('/RiwayatPenyakit')),
 				array('label'=>'Pelanggaran', 'url'=>array('/PencatatanPelanggaran')),
 				array('label'=>'Perizinan', 'url'=>array('/Perizinan')),
-				array('label'=>'Data Santri', 'url'=>array('/Santri')),
-				//array('label'=>'Site', 'url'=>array('/Site')),
-				array('label'=>'User', 'url'=>array('/User')),
 				array('label'=>'Tahun Ajaran', 'url'=>array('/TahunAjaran')),
 				array('label'=>'Mata Pelajaran', 'url'=>array('/MataPelajaran')),
+				array('label'=>'Riwayat Penyakit', 'url'=>array('/RiwayatPenyakit')),
 				//array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
 		)); ?>
--- protected/views/santri/admin.php
+++ protected/views/santri/admin.php
@@ -3,13 +3,13 @@
 /* @var $model Santri */
 
 $this->breadcrumbs=array(
-	'Santris'=>array('index'),
-	'Manage',
+	'Data Santri'=>array('index'),
+	'Atur Data Santri',
 );
 
 $this->menu=array(
-	array('label'=>'List Santri', 'url'=>array('index')),
-	array('label'=>'Create Santri', 'url'=>array('create')),
+	array('label'=>'Daftar Santri', 'url'=>array('index')),
+	array('label'=>'Buat Data Santri', 'url'=>array('create')),
 );
 
 Yii::app()->clientScript->registerScript('search', "
@@ -26,12 +26,12 @@ $('.search-form form').submit(function(){
 ");
 ?>
 
-<h1>Manage Santris</h1>
+<h1>Atur Data Santri</h1>
 
-<p>
+<!-- <p>
 You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
 or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
+</p> -->
 
 <?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
 <div class="search-form" style="display:none">
@@ -45,12 +45,11 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
 	'columns'=>array(
-		'nip',
+
 		'nis',
-		'nik',
-		'nisn',
+	
 		'nama_lengkap',
-		'nama_kecil',
+	
 		/*
 		'status',
 		'alamat',
