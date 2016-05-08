500640aad93f59789b78829a9948893b2d10462f
fauzanmajid <fauzanmajid120495@gmail.com>
2016-05-08 22:58:03 +0700

    punya ojan

--- protected/controllers/PerwalianController.php
+++ protected/controllers/PerwalianController.php
@@ -1,6 +1,6 @@
 <?php
 
-class SantriController extends Controller
+class PerwalianController extends Controller
 {
 	/**
 	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
@@ -26,18 +26,18 @@ class SantriController extends Controller
 	 */
 	public function accessRules()
 	{
-		return array(	
+		return array(
+			array('allow',  // allow all users to perform 'index' and 'view' actions
+				'actions'=>array('index','view'),
+				'users'=>array('*'),
+			),
+			array('allow', // allow authenticated user to perform 'create' and 'update' actions
+				'actions'=>array('create','update'),
+				'users'=>array('@'),
+			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-                'actions' => array('index','view','admin','delete','create','update'),
-                'expression' => function(UserWeb $user){
-                	return $user->isAdmin();}
-                	),
-              
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-                'actions' => array('riwayatPenyakit', 'perizinan', 'pelanggaran', 'prestasi'),
-                'expression' => function(UserWeb $user) {
-                /* @var $user UserWeb */
-                return $user->isKesiswaan();}
+				'actions'=>array('admin','delete'),
+				'users'=>array('admin'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
@@ -62,28 +62,16 @@ class SantriController extends Controller
 	 */
 	public function actionCreate()
 	{
-		$model=new Santri;
+		$model=new Perwalian;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Santri']))
+		if(isset($_POST['Perwalian']))
 		{
-			$model->attributes=$_POST['Santri'];
-			/*
-		
-			$fileSource = Yii::getPathOfAlias('webroot').'/img/';
-			$imgTem = CUploadedFile::getInstance($model,'foto_url');
-			$imgTem->saveAs($fileSource.$imgTem);
-			$model->foto_url = $imgTem;
-			*/		
+			$model->attributes=$_POST['Perwalian'];
 			if($model->save())
-				{
-			
-
-
-				$this->redirect(array('view','id'=>$model->nip));
-				}
+				$this->redirect(array('view','id'=>$model->id));
 		}
 
 		$this->render('create',array(
@@ -103,15 +91,11 @@ class SantriController extends Controller
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
 
-		if(isset($_POST['Santri']))
+		if(isset($_POST['Perwalian']))
 		{
-			 
-			$model->attributes=$_POST['Santri'];
-			
+			$model->attributes=$_POST['Perwalian'];
 			if($model->save())
-			
-				$this->redirect(array('view','id'=>$model->nip));
-			
+				$this->redirect(array('view','id'=>$model->id));
 		}
 
 		$this->render('update',array(
@@ -138,18 +122,9 @@ class SantriController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('Santri');
-
-		$model=new Santri('search');
-		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Santri']))
-			$model->attributes=$_GET['Santri'];
-
+		$dataProvider=new CActiveDataProvider('Perwalian');
 		$this->render('index',array(
 			'dataProvider'=>$dataProvider,
-			'model'=>$model,
-
-
 		));
 	}
 
@@ -158,10 +133,10 @@ class SantriController extends Controller
 	 */
 	public function actionAdmin()
 	{
-		$model=new Santri('search');
+		$model=new Perwalian('search');
 		$model->unsetAttributes();  // clear any default values
-		if(isset($_GET['Santri']))
-			$model->attributes=$_GET['Santri'];
+		if(isset($_GET['Perwalian']))
+			$model->attributes=$_GET['Perwalian'];
 
 		$this->render('admin',array(
 			'model'=>$model,
@@ -172,12 +147,12 @@ class SantriController extends Controller
 	 * Returns the data model based on the primary key given in the GET variable.
 	 * If the data model is not found, an HTTP exception will be raised.
 	 * @param integer $id the ID of the model to be loaded
-	 * @return Santri the loaded model
+	 * @return Perwalian the loaded model
 	 * @throws CHttpException
 	 */
 	public function loadModel($id)
 	{
-		$model=Santri::model()->findByPk($id);
+		$model=Perwalian::model()->findByPk($id);
 		if($model===null)
 			throw new CHttpException(404,'The requested page does not exist.');
 		return $model;
@@ -185,50 +160,14 @@ class SantriController extends Controller
 
 	/**
 	 * Performs the AJAX validation.
-	 * @param Santri $model the model to be validated
+	 * @param Perwalian $model the model to be validated
 	 */
 	protected function performAjaxValidation($model)
 	{
-		if(isset($_POST['ajax']) && $_POST['ajax']==='santri-form')
+		if(isset($_POST['ajax']) && $_POST['ajax']==='perwalian-form')
 		{
 			echo CActiveForm::validate($model);
 			Yii::app()->end();
 		}
-	}
-
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionRiwayatPenyakit($id)
-	{
-		$this->render('riwayat-penyakit',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
-
-	/**
-	 * Displays a particular model.
-	 * @param integer $id the ID of the model to be displayed
-	 */
-	public function actionPerizinan($id)
-	{
-		$this->render('perizinan',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
-
-	public function actionPelanggaran($id)
-	{
-		$this->render('pelanggaran',array(
-			'model'=>$this->loadModel($id),
-		));
-	}
-
-	public function actionPrestasi($id)
-	{
-		$this->render('prestasi',array(
-			'model'=>$this->loadModel($id),
-		));
 	}
 }
---
+++ protected/views/perwalian/view.php
@@ -0,0 +1,37 @@
+<?php
+/* @var $this PerwalianController */
+/* @var $model Perwalian */
+
+$this->breadcrumbs=array(
+	'Perwalians'=>array('index'),
+	$model->id,
+);
+
+$this->menu=array(
+	array('label'=>'List Perwalian', 'url'=>array('index')),
+	array('label'=>'Create Perwalian', 'url'=>array('create')),
+	array('label'=>'Update Perwalian', 'url'=>array('update', 'id'=>$model->id)),
+	array('label'=>'Delete Perwalian', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
+	array('label'=>'Manage Perwalian', 'url'=>array('admin')),
+);
+?>
+
+<h1>View Perwalian #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('zii.widgets.CDetailView', array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'nip_santri',
+		'status',
+		'nama',
+		'tempat_lahir',
+		'tanggal_lahir',
+		'agama',
+		'alamat',
+		'no_telepon',
+		'pekerjaan',
+		'pendidikan',
+		'penghasilan',
+	),
+)); ?>
