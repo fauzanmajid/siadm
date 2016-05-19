cae4d2c9f9a6af6befe141377615033781a9c849
Kahfi Kurnia <Kahfi Kurnia>
2016-05-19 03:43:01 +0100

    ojan

---
+++ protected/controllers/UnduhController.php
@@ -0,0 +1,113 @@
+<?php
+
+class UnduhController extends Controller
+{
+	/**
+	 * @var string the default layout for the views. Defaults to '//layouts/column2', meaning
+	 * using two-column layout. See 'protected/views/layouts/column2.php'.
+	 */
+	public $layout='//layouts/column2';
+
+	/**
+	 * @return array action filters
+	 */
+	public function filters()
+	{
+		return array(
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
+		);
+	}
+
+	/**
+	 * Specifies the access control rules.
+	 * This method is used by the 'accessControl' filter.
+	 * @return array access control rules
+	 */
+	public function accessRules()
+	{
+		return array(	
+			array('allow', // allow admin user to perform 'admin' and 'delete' actions
+
+                'actions' => array('unduhtransaksipengeluaran', 'excelpengeluaran'),
+                //'deniedCallback' => array($this,'gotoLogin'),             
+                'expression' => function(UserWeb $user) {
+                /* @var $user UserWeb */
+                return $user->isBendahara();}
+			),
+
+			
+			array('deny',  // deny all users
+				'users'=>array('*'),
+				'deniedCallback' => function() { Yii::app()->controller->redirect(array ('/site/index')); }
+			),
+		);
+	}
+
+	
+	public function actionUnduhTransaksiPengeluaran()
+	{
+		$model = new TransaksiPengeluaran;
+		$model->scenario = 'unduh-laporan-keuangan';
+
+		if(isset($_POST['TransaksiPengeluaran'])) {
+		
+			$tanggal_awal = $_POST['TransaksiPengeluaran']['tanggal_awal'];
+			$tanggal_akhir = $_POST['TransaksiPengeluaran']['tanggal_akhir'];
+			$jenis = $_POST['TransaksiPengeluaran']['jenis'];
+			if($jenis= "Pengeluaran"){
+			$this->redirect(Yii::app()->createUrl('unduh/excelPengeluaran', array('awal' => $tanggal_awal, 'akhir' => $tanggal_akhir)));
+			}
+		}
+
+		$this->render('/unduh/unduh-laporan-keuangan',array(
+			'model' => $model,
+		));
+	}
+
+
+
+	public function actionExcelPengeluaran($awal = null, $akhir = null){
+        
+        //Some data
+
+        $criteria = new CDbCriteria();
+        if ($awal != null){
+			$criteria->compare('tanggal_pengeluaran','>='.$awal);	
+		}
+
+		if ($akhir != null){
+			$criteria->compare('tanggal_pengeluaran','<='.$akhir);
+		}
+		
+		$models = TransaksiPengeluaran::model()->findAll($criteria);
+		
+		$transaksi = [];
+		foreach($models as $model) {
+		    $transaksi[$model->kode] = $model->attributes;
+		}
+
+        $report = new YiiReport(array('template'=> 'pengeluaran.xls'));
+        
+        $report->load(array(
+                array(
+                    'id' => 'judul',
+                    'data' => array(
+                        'name' => 'LAPORAN PENGELUARAN SANTRI POPNPES AL-LATHIFA MULIA'
+                    )
+                ),
+                array(
+                    'id'=>'kode',
+                    'repeat'=>true,
+                    'data'=>$transaksi,
+                    'minRows'=>2
+                ),
+            )
+        );
+        
+         echo $report->render('excel5', 'LaporanPengeluaran');
+        // echo $report->render('excel2007', 'Students');
+        // echo $report->render('pdf', 'daftar santri');
+        
+    }//actionExcel method end
+}
---
+++ protected/models/LaporanPengeluaran.php
@@ -0,0 +1,102 @@
+<?php
+
+/**
+ * This is the model class for table "kelas".
+ *
+ * The followings are the available columns in table 'kelas':
+ * @property integer $id
+ * @property string $nama
+ * @property string $jenjang
+ *
+ * The followings are the available model relations:
+ * @property EnrollmentPelajaran[] $enrollmentPelajarans
+ */
+class LaporanPengeluaran extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'transaksi_pengeluaran';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('jenis_kelamin', 'required'),
+			array('jumlah', 'integerOnly'=>true),
+			
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'jenis_kelamin' => 'jenis_kelamin',
+			'jumlah' => 'jumlah'
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('jenis_kelamin',$this->jenis_kelamin,true);
+		$criteria->compare('jumlah',$this->jumlah,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Kelas the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	public function getJumlahInteger() {
+        return intval($this->jumlah);
+    }
+    
+}
